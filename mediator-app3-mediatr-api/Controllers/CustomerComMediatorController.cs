1c2ef1b12c3906a957e1ff2a3519e998a82ee5bc
joaoveasey <jovepedro@gmail.com>
2024-09-30 18:56:27 -0300

    feat: implementando controllers ( com mediator e sem mediator ) para entendermos a diferença

---
+++ mediator-app3-mediatr-api/Controllers/CustomerComMediatorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediator_app3_mediatr_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerComMediatorController : ControllerBase
+    {
+    }
+}
---
+++ mediator-app3-mediatr-api/Controllers/CustomerSemMediatorController.cs
@@ -0,0 +1,42 @@
+﻿using mediator_app3_mediatr_api.Models;
+using mediator_app3_mediatr_api.Repositories;
+using mediator_app3_mediatr_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediator_app3_mediatr_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerSemMediatorController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IValidationService _validationService;
+
+        public CustomerSemMediatorController(ICustomerRepository customerRepository, IValidationService validationService)
+        {
+            _customerRepository = customerRepository;
+            _validationService = validationService;
+        }
+
+        [HttpGet("get-customer/{customerId:Guid}")]
+        public async Task<Customer> GetCustomer(Guid customerId)
+        {
+            _validationService.Validate<Guid>(customerId);
+
+            return await _customerRepository.GetCustommer(customerId);
+        }
+
+        [HttpPost("create-customer")]
+        public async Task<Guid> CreateCustomer([FromBody] Customer newCustomer)
+        {
+            _validationService.Validate<Customer>(newCustomer);
+
+            var customer = new Customer
+            {
+                Name = newCustomer.Name
+            };
+
+            return await _customerRepository.CreateCustomer(customer);
+        }
+    }
+}
